caf0e542d2a9ef2f683fa487e3a171f78f7d2310
Ayodya-Erandi <ayodyaerandi2018@gmail.com>
2023-08-26 22:53:12 +0530

    Add initial project structure

---
+++ src/test/java/com/example/demo/user/app/DemoUserAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo.user.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoUserAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
